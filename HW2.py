66ec9553dd8478675714fa1941eefb81c8c5bc98
walshg3 <walshg3@go.stockton.edu>
2024-02-07 22:50:34 -0800

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,35 @@
+# HW2.py
+# Author:
+
+
+# Question 1:
+# Write some code that prompts the user for their age. Depending on the input:
+
+# If the age is less than 13, print "You are a child."
+# If the age is between 13 and 19, print "You are a teenager."
+# If the age is 20 or older, print "You are an adult."
+
+
+# Question 2:
+# Write some code to display the following pattern using a for or while loop:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# Question 3:
+# Write some code that prompts the user to input 10 numbers. After all the numbers are inputted, the program should display:
+
+# The highest number.
+# The lowest number.
+# The average of all the numbers.
+
+# Hint 1: You will need to use a for loop and a counter variable to keep track of the total sum of the numbers.
+# Hint 2: You will need to use an if statement to keep track of the highest and lowest numbers.
+
+
+# Question 4:
+# Vowel Counter - Write some code that prompts the user to enter a string. The program should then display the number of vowels in the string. IE. If the user enters "Hello World", the program should display 3.
+# the vowels are a, e, i, o, u
+# Hint: convert the string to lowercase and use a for loop with a counter variable and an if statement
